45954b15f8988f0fb6fcbd855ef35a8f6ced3df9
JoeMcCleery <mccleery.joseph@gmail.com>
2024-05-30 21:49:17 +0800

    Fix pre-rendering bug

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -1,5 +1,6 @@
 import PageContainer from "@/components/pageContainer";
 import BoardContainer from "@/components/sudoku/boardContainer";
+import { Suspense } from "react";
 
 export const metadata = {
   title: "Sudoku App - Play",
@@ -9,7 +10,9 @@ export const metadata = {
 export default function Page() {
   return (
     <PageContainer>
-      <BoardContainer />
+      <Suspense>
+        <BoardContainer />
+      </Suspense>
     </PageContainer>
   );
 }
