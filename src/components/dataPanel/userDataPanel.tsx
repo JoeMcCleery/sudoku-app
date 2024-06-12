48ed934df27cb233b700064075538319f03695cd
JoeMcCleery <mccleery.joseph@gmail.com>
2024-06-12 15:18:06 +0800

    bugfix hydration error

--- src/components/dataPanel/userDataPanel.tsx
+++ src/components/dataPanel/userDataPanel.tsx
@@ -1,15 +1,21 @@
 "use client";
 
-import { useReducer } from "react";
+import { useEffect, useState } from "react";
 import { getAllBoardData } from "@/data/localStorage";
 import { GamesList } from "@/components/dataPanel/gamesList";
 import DangerZone from "@/components/dataPanel/dangerZone";
 import GraphSection from "@/components/dataPanel/graphSection";
 
 export function UserDataPanel() {
-  const [, forceUpdate] = useReducer((x) => x + 1, 0);
+  const [data, setData] = useState<BoardData[]>([]);
 
-  const data = getAllBoardData();
+  useEffect(() => {
+    setData(getAllBoardData());
+  }, []);
+
+  function onReset() {
+    setData([]);
+  }
 
   return (
     <div className="grid gap-8">
@@ -17,7 +23,7 @@ export function UserDataPanel() {
       <GamesList data={data} />
       <DangerZone
         data={data}
-        onReset={forceUpdate}
+        onReset={onReset}
       />
     </div>
   );
